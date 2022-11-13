9e96dd426bbf6de69963b4343860fe2e8cdcff89
kspipa <117585525+kspipa@users.noreply.github.com>
2022-11-13 20:55:31 +0300

    Add files via upload

--- test.c
+++ test.c
@@ -1,24 +1,17 @@
 #include <linux/module.h>
-#include <linux/netfilter_ipv4.h> 
 #include <linux/kernel.h>
-#include <linux/init.h> 
 #include <linux/netlink.h>
 #include <net/netlink.h>
 #include <net/net_namespace.h>
-
-MODULE_AUTHOR("pipa"); 
-MODULE_DESCRIPTION("easy firewall");
-MODULE_LICENSE("GPL");
 
 /* Protocol family, consistent in both kernel prog and user prog. */
 #define MYPROTO NETLINK_USERSOCK
 /* Multicast group, consistent in both kernel prog and user prog. */
 #define MYGRP 21
 
-
 static struct sock *nl_sk = NULL;
 
-static void send_to_user(struct sk_buff *skbf)
+static void send_to_user(struct sk_buff *negr)
 {
     struct sk_buff *skb;
     struct nlmsghdr *nlh;
@@ -34,7 +27,7 @@ static void send_to_user(struct sk_buff *skbf)
     }
 
     nlh = nlmsg_put(skb, 0, 1, NLMSG_DONE, msg_size + 1, 0);
-    nlh->nlmsg_type = (__u16)skbf;
+    nlh->nlmsg_type = (__u16)skb;
 
     pr_info("Sending skb.\n");
     res = nlmsg_multicast(nl_sk, skb, 0, MYGRP, GFP_KERNEL);
@@ -44,34 +37,28 @@ static void send_to_user(struct sk_buff *skbf)
         pr_info("Success.\n");
 }
 
-
-static struct nf_hook_ops *nfho = NULL;
-
-unsigned int hook_funcion(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+static int __init hello_init(void)
 {
-    send_to_user(skb);
-    return NF_DROP;
-}
+    pr_info("Inserting hello module.\n");
 
-static int __init hook_init(void)
-{
-    nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
-    nfho->hook = (nf_hookfn*)hook_funcion;
-    nfho->pf = NFPROTO_IPV4;
-    nfho->hooknum = NF_INET_LOCAL_IN;
-    nfho->priority = NF_IP_PRI_MANGLE;
     nl_sk = netlink_kernel_create(&init_net, MYPROTO, NULL);
-    nf_register_net_hook(&init_net, nfho);
-    printk("xmurp-ua start\n");
+    if (!nl_sk) {
+        pr_err("Error creating socket.\n");
+        return -10;
+    }
+
+    send_to_user();
+
+    netlink_kernel_release(nl_sk);
     return 0;
 }
 
-static void __exit hook_exit(void)
+static void __exit hello_exit(void)
 {
-    nf_unregister_net_hook(&init_net, nfho);
-    netlink_kernel_release(nl_sk);
-    printk("xmurp-ua stop\n");
+    pr_info("Exiting hello module.\n");
 }
 
-module_init(hook_init);
-module_exit(hook_exit);
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");

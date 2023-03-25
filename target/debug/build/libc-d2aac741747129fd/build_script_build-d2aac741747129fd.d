e99ac60b575ee2631efb08868988c8d64e5180e3
kspipa <yyyyyyy5yyyy@gmail.com>
2023-03-25 15:12:36 +0300

    .

--- target/debug/build/libc-d2aac741747129fd/build_script_build-d2aac741747129fd.d
+++
@@ -1,5 +0,0 @@
-/home/pipa/Documents/Code/rust/Firework/target/debug/build/libc-d2aac741747129fd/build_script_build-d2aac741747129fd: /home/pipa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.139/build.rs
-
-/home/pipa/Documents/Code/rust/Firework/target/debug/build/libc-d2aac741747129fd/build_script_build-d2aac741747129fd.d: /home/pipa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.139/build.rs
-
-/home/pipa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.139/build.rs:

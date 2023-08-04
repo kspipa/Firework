5766a567e4de309962dec33c586bb9b4a33355ba
kspipa <yyyyyyy5yyyy@gmail.com>
2023-08-04 22:31:53 +0300

    modified:   src/main.rs

--- Makefile
+++ Makefile
@@ -3,19 +3,6 @@ module_path = $(path)/modules
 all:
 	cargo build
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD)/kernel_module modules
-uninstall:
-	sudo rm -rf $(path)
-	sudo rm -rf /usr/bin/frwk
-install:
-	sudo python3 src/tools/useradd.py
-	mkdir $(path) $(module_path)
-	cp target/debug/Firework $(path)
-	cp -r kernel_module $(path)
-	cp -r src/moduleManager $(path)
-	sudo chown firework:firework $(path) -R
-	sudo chmod a+x $(path) -R
-	sudo cp start.sh /usr/bin/frwk && sudo chmod o+x /usr/bin/frwk
-	export FRWK_PATH=$(path)
 clean:
 	rm -rf target
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD)/kernel_module clean
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-mod firework_lib;
-use firework_lib::*;
 use nfq::{Queue, Verdict};
 fn main() -> std::io::Result<()>{
-    let mut k = shmemCell::init("strongnigga");
-    let mut seccell = shmemCell::init("strongnigga2");
     let mut queue = Queue::open()?;
     queue.bind(0)?;
     let mut new_msg: nfq::Message;
     loop{
         new_msg = queue.recv()?;
-        k.setpack(new_msg.get_payload(), (new_msg.get_packet_id() as u8));
-        let res = seccell.waitcode();
-        if res.1 == 0{
-            new_msg.set_verdict(Verdict::Drop)
-        }
-        else {
-            new_msg.set_verdict(Verdict::Accept)
-        }
+        new_msg.set_verdict(Verdict::Accept);
         queue.verdict(new_msg);
     }
     Ok(())
